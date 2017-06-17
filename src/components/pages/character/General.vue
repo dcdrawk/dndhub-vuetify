adef820d353d5e95afc8e66db8453301089d4705
devin <devin@thewebdev.ca>
2017-06-17 15:30:42 -0700

    updated general component

---
+++ src/components/pages/character/General.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>Component</div>
+</template>
+
+<script>
+export default {
+
+  // Name
+  name: 'component',
+
+  // Components
+  components: {},
+
+  // Props
+  props: {},
+
+  // Data
+  data () {
+    return {}
+  },
+
+  // Computed
+  computed: {
+    computedProperty () {
+      return 'something'
+    }
+  },
+
+  // Watch
+  watch: {
+    watchProperty (newValue, oldValue) {
+      return newValue
+    }
+  },
+
+  // Methods
+  methods: {
+    myFunction () {
+      return 'something'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
